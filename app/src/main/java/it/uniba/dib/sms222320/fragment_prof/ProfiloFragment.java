9bdfe3634c30b4780af73bce2c217692ad64fbaf
michelebissanti <michele.bissanti01@gmail.com>
2022-12-13 19:18:01 +0100

    mi ero stancato: - bisogna fare l inserimento del prof su firebase - far funzionare il pulsante di log out - testare la registrazione

--- app/src/main/java/it/uniba/dib/sms222320/fragment_prof/ProfiloFragment.java
+++ app/src/main/java/it/uniba/dib/sms222320/fragment_prof/ProfiloFragment.java
@@ -1,13 +1,21 @@
 package it.uniba.dib.sms222320.fragment_prof;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import java.io.*;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import it.uniba.dib.sms222320.LoginActivity;
+import it.uniba.dib.sms222320.MainProfActivity;
 import it.uniba.dib.sms222320.R;
 
 public class ProfiloFragment extends Fragment {
@@ -19,6 +27,26 @@ public class ProfiloFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profilo, container, false);
+        View view = inflater.inflate(R.layout.fragment_profilo, container, false);
+
+        Button esciBtn = view.findViewById(R.id.logout);
+
+        // Set on Click Listener on Sign-in button
+        esciBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                FirebaseAuth.getInstance().signOut();
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    Log.d("1", "NON FUNZIONA");
+                } else {
+                    Log.d("1", "mi sono scollegato");
+                }
+            }
+        });
+
+        return view;
     }
 }
