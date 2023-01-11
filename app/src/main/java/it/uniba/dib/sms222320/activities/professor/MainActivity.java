9de24c5cc00cf00752d5f81076c7bfc36bc3da57
DomeCrista <98021786+DomeCrista@users.noreply.github.com>
2023-01-11 16:07:05 +0100

    feat : add text and widget

--- app/src/main/java/it/uniba/dib/sms222320/activities/professor/MainActivity.java
+++ app/src/main/java/it/uniba/dib/sms222320/activities/professor/MainActivity.java
@@ -24,24 +24,21 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import it.uniba.dib.sms222320.R;
 import it.uniba.dib.sms222320.activities.LoginActivity;
-import it.uniba.dib.sms222320.activities.RegisterActivity;
-import it.uniba.dib.sms222320.activities.SignedInActivity;
 import it.uniba.dib.sms222320.databinding.MainProfessorBinding;
 import it.uniba.dib.sms222320.models.Professor;
-import it.uniba.dib.sms222320.models.User;
 
 public class MainActivity extends AppCompatActivity {
 
     MainProfessorBinding binding;
     private FirebaseAuth mAuth;
 
-    private EditText formName, formSurname, formDateBirth, formCity, formAddress;
+    private EditText formName, formSurname, formDateBirth, formCity, formAddress, formTelephone;
 
-    private TextView usernameText, corsoText, matricolaText;
+    private TextView usernameText, corsoText, matricolaText, welcomeText;
 
     private DatabaseReference mDatabase;
 
-    private Button modProfile, saveProfile;
+    private Button modProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance("https://laureapp-fbf2b-default-rtdb.europe-west1.firebasedatabase.app").getReference();
 
+        //Profile Fragment
         usernameText = findViewById(R.id.usernameText);
         corsoText = findViewById(R.id.corsoText);
         matricolaText = findViewById(R.id.matricolaText);
@@ -61,56 +59,27 @@ public class MainActivity extends AppCompatActivity {
         formDateBirth = findViewById(R.id.formDateBirth);
         formCity = findViewById(R.id.formCity);
         formAddress = findViewById(R.id.formAddress);
+        formTelephone = findViewById(R.id.formTelephone);
 
+        //Home Fragment
+        welcomeText = findViewById(R.id.welcomeText);
+
+        homeClicked();
         replaceFragment(new HomeFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
             switch(item.getItemId()) {
                 case R.id.home:
+
+                    homeClicked();
                     replaceFragment(new HomeFragment());
                     break;
                 case R.id.profile:
 
-                    mDatabase.child("users").child(mAuth.getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DataSnapshot> task) {
-                            if (!task.isSuccessful()) {
-                                Log.e("firebase", "Error Getting Data", task.getException());
-                            } else {
-                                Log.d("firebase", String.valueOf(task.getResult().getValue()));
-
-                                Professor user = task.getResult().getValue(Professor.class);
-
-                                usernameText = findViewById(R.id.usernameText);
-                                matricolaText = findViewById(R.id.matricolaText);
-
-                                formName = findViewById(R.id.formName);
-                                formSurname = findViewById(R.id.formSurname);
-                                formDateBirth = findViewById(R.id.formDateBirth);
-                                formCity = findViewById(R.id.formCity);
-                                formAddress = findViewById(R.id.formAddress);
-
-                                formName.setEnabled(false);
-                                formSurname.setEnabled(false);
-                                formDateBirth.setEnabled(false);
-                                formCity.setEnabled(false);
-                                formAddress.setEnabled(false);
-
-                                usernameText.setText(user.getUsername());
-                                matricolaText.setText(user.getMatricola());
-
-                                formName.setText(user.getName());
-                                formSurname.setText(user.getSurname());
-                                formDateBirth.setText(user.getBirthDate());
-                                formCity.setText(user.getCity());
-                                formAddress.setText(user.getAddress());
-
-                            }
-                        }
-                    });
-
+                    profileClicked();
                     replaceFragment(new ProfileFragment());
+
                     break;
                 case R.id.ricevimenti:
                     replaceFragment(new RicevimentiFragment());
@@ -128,6 +97,69 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void homeClicked() {
+
+        mDatabase.child("users").child(mAuth.getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(!task.isSuccessful()) {
+                    Log.e("firebase", "Error Getting Data", task.getException());
+                } else {
+
+                    Professor user = task.getResult().getValue(Professor.class);
+
+                    welcomeText = findViewById(R.id.welcomeText);
+
+                    welcomeText.setText("Bentornato, " + user.getUsername() + "!");
+
+                }
+            }
+        });
+    }
+
+    private void profileClicked() {
+        mDatabase.child("users").child(mAuth.getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error Getting Data", task.getException());
+                } else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+
+                    Professor user = task.getResult().getValue(Professor.class);
+
+                    usernameText = findViewById(R.id.usernameText);
+                    matricolaText = findViewById(R.id.matricolaText);
+
+                    formName = findViewById(R.id.formName);
+                    formSurname = findViewById(R.id.formSurname);
+                    formDateBirth = findViewById(R.id.formDateBirth);
+                    formCity = findViewById(R.id.formCity);
+                    formAddress = findViewById(R.id.formAddress);
+                    formTelephone = findViewById(R.id.formTelephone);
+
+                    formName.setEnabled(false);
+                    formSurname.setEnabled(false);
+                    formDateBirth.setEnabled(false);
+                    formCity.setEnabled(false);
+                    formAddress.setEnabled(false);
+                    formTelephone.setEnabled(false);
+
+
+                    usernameText.setText(user.getUsername());
+                    matricolaText.setText(user.getMatricola());
+
+                    formName.setText(user.getName());
+                    formSurname.setText(user.getSurname());
+                    formDateBirth.setText(user.getBirthDate());
+                    formCity.setText(user.getCity());
+                    formAddress.setText(user.getAddress());
+                    formTelephone.setText(user.getTelephone());
+
+                }
+            }
+        });
+    }
 
     private void replaceFragment(Fragment fragment){
 
@@ -172,6 +204,7 @@ public class MainActivity extends AppCompatActivity {
             mDatabase.child("users").child(mAuth.getUid()).child("birthDate").setValue(formDateBirth.getText().toString());
             mDatabase.child("users").child(mAuth.getUid()).child("city").setValue(formCity.getText().toString());
             mDatabase.child("users").child(mAuth.getUid()).child("address").setValue(formAddress.getText().toString());
+            mDatabase.child("users").child(mAuth.getUid()).child("telephone").setValue(formTelephone.getText().toString());
             Toast.makeText(MainActivity.this, "Modifiche apportate con Successo.",
                     Toast.LENGTH_LONG).show();
         }
@@ -179,7 +212,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onSaveProfileClick(View view) {
-
-    }
 }
